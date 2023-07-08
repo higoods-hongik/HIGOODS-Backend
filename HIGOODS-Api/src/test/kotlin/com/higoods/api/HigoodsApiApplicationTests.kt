24fe02d6710246f13f3406f59757de755849e143
이채린 (Bryn) <67696767+cofls6581@users.noreply.github.com>
2023-07-09 04:28:39 +0900

    feat: ktlint 추가 (#2)

--- HIGOODS-Api/src/test/kotlin/com/higoods/api/HigoodsApiApplicationTests.kt
+++ HIGOODS-Api/src/test/kotlin/com/higoods/api/HigoodsApiApplicationTests.kt
@@ -9,5 +9,4 @@ class HigoodsApiApplicationTests {
     @Test
     fun contextLoads() {
     }
-
 }
--- HIGOODS-Infra/build.gradle.kts
+++ HIGOODS-Infra/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
-dependencies{
+dependencies {
     implementation(project(":HIGOODS-Common"))
 
-    api ("org.springframework.boot:spring-boot-starter-data-redis")
-    api ("org.redisson:redisson:3.19.0")
+    api("org.springframework.boot:spring-boot-starter-data-redis")
+    api("org.redisson:redisson:3.19.0")
 }
 
 tasks {
