7ac608a22fc12262518d97cee90dc97eb5fa4f76
bryn <cofls6581@naver.com>
2023-07-11 01:58:08 +0900

    refactor: 자바 모듈 코틀린으로 변경 및 인터페이스화

---
+++ HIGOODS-Common/src/test/kotlin/com/higoods/common/HigoodsCommonApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.higoods.common
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HigoodsCommonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
