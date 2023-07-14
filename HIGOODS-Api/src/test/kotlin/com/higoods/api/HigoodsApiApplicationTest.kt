6403a0517db9444afd6f9b12ccc17e254256fba8
bryn <cofls6581@naver.com>
2023-07-14 21:29:59 +0900

    refactor: 불필요한 파일 제거 및 함수명 수정

---
+++ HIGOODS-Api/src/test/kotlin/com/higoods/api/HigoodsApiApplicationTest.kt
@@ -0,0 +1,9 @@
+package com.higoods.api
+
+import io.kotest.core.spec.style.StringSpec
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HigoodsApiApplicationTest : StringSpec({
+    "contextLoads" {}
+})
