af0c1be1c02e24eed7cb2e20333ad526de205160
이채린 (Bryn) <67696767+cofls6581@users.noreply.github.com>
2023-07-16 19:46:59 +0900

    feat: jacoco+sonar 설정 (#34)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,27 @@ plugins {
     kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION apply false
     id("org.jlleitschuh.gradle.ktlint") version PluginVersions.KTLINT_VERSIOM
     kotlin("kapt") version PluginVersions.KAPT_VERSION apply false
+    id("org.sonarqube") version PluginVersions.SONAR_VERSION
+    id("jacoco")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "higoods-hongik_HIGOODS-Backend")
+        property("sonar.organization", "higoods-api")
+        property("sonar.host.url", "https://sonarcloud.io")
+        // sonar additional settings
+        property("sonar.sources", "src")
+        property("sonar.language", "Kotlin")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.test.inclusions", "**/*Test.java")
+        property("sonar.exclusions", "**/test/**, **/Q*.kt, **/*Doc*.kt, **/resources/** ,**/*Application*.kt , **/*Config*.kt, **/*Dto*.kt, **/*Request*.kt, **/*Response*.kt ,**/*Exception*.kt ,**/*ErrorCode*.kt")
+        property("sonar.java.coveragePlugin", "jacoco")
+    }
 }
 
 allprojects {
@@ -39,6 +56,7 @@ subprojects {
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "jacoco")
 
     dependencies {
         implementation(Dependencies.SPRING_DATA_JPA)
@@ -57,5 +75,42 @@ subprojects {
 
     tasks.getByName<Jar>("jar") {
         enabled = false
+    }
+    tasks.test {
+        finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    }
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test) // tests are required to run before generating the report
+        reports {
+            html.required.set(true) // html 설정
+            csv.required.set(true) // csv 설정
+            xml.required.set(true)
+            xml.outputLocation.set(File("$buildDir/reports/jacoco.xml"))
+        }
+        classDirectories.setFrom(
+            files(
+                classDirectories.files.map {
+                    fileTree(it) { // 테스트 커버리지 측정 제외 목록
+                        exclude(
+                            "**/*Application*",
+                            "**/*Config*",
+                            "**/*Dto*",
+                            "**/*Request*",
+                            "**/*Response*",
+                            "**/*Interceptor*",
+                            "**/*Exception*",
+                            "**/Q*"
+                        ) // QueryDsl 용이나 Q로 시작하는 클래스 뺄 위험 존재
+                    }
+                }
+            )
+        )
+    }
+
+    sonarqube {
+        properties {
+            property("sonar.java.binaries", "$buildDir/classes")
+            property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco.xml")
+        }
     }
 }
