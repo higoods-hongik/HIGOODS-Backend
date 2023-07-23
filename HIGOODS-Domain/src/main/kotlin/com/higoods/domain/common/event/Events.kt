3e12a4526b923bd53f2d483d558e1ecd3ede57aa
이찬진 <water0641@naver.com>
2023-07-23 20:47:48 +0900

    feat : DomainEvent aop 추가

---
+++ HIGOODS-Domain/src/main/kotlin/com/higoods/domain/common/event/Events.kt
@@ -0,0 +1,23 @@
+package com.higoods.domain.common.event
+
+import org.springframework.context.ApplicationEventPublisher
+
+class Events {
+
+    companion object {
+
+        @JvmStatic
+        val publisherLocal = ThreadLocal<ApplicationEventPublisher>()
+        fun raise(event: DomainEvent) {
+            publisherLocal.get().publishEvent(event)
+        }
+
+        fun setPublisher(publisher: ApplicationEventPublisher) {
+            publisherLocal.set(publisher)
+        }
+
+        fun reset() {
+            publisherLocal.remove()
+        }
+    }
+}
