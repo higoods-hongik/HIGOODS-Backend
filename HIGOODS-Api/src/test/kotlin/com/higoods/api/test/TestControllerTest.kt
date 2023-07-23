18cdfc9ab14e65006873d83999117b4912c12347
bryn <cofls6581@naver.com>
2023-07-24 07:37:01 +0900

    fix: openapi3에 스니펫 내용 안들어가는 오류 해결

--- HIGOODS-Api/src/test/kotlin/com/higoods/api/test/TestControllerTest.kt
+++ HIGOODS-Api/src/test/kotlin/com/higoods/api/test/TestControllerTest.kt
@@ -1,14 +1,14 @@
 package com.higoods.api.test
 
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder
 import com.higoods.api.common.BaseControllerTest
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.http.MediaType
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
-import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.time.LocalDate
 
@@ -25,22 +25,24 @@ class TestControllerTest : BaseControllerTest() {
             )
             every { testService.test("test") } returns testResponse
 
-            val result = mockMvc.perform(
+            mockMvc.perform(
                 get("/v1/test")
                     .contentType(MediaType.APPLICATION_JSON)
                     .accept(MediaType.APPLICATION_JSON)
             )
-            result.andExpect(status().isOk())
-            result.andDo(
-                document(
-                    "테스트 API",
-                    responseFields(
-                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("id"),
-                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
-                        fieldWithPath("currentTime").type(JsonFieldType.STRING).description("현재시각")
+                .andExpect(status().isOk())
+                .andDo(
+                    document(
+                        "테스트 API",
+                        ResourceSnippetParametersBuilder()
+                            .description("이번엔 되겠지")
+                            .responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("currentTime").type(JsonFieldType.STRING).description("현재시각")
+                            )
                     )
                 )
-            )
         }
     }
 }
