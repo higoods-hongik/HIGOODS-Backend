cb5b3010944640da06050c30ab330004957236f8
bryn <cofls6581@naver.com>
2023-08-06 22:35:36 +0900

    feat: 주문 도메인 추가

---
+++ HIGOODS-Domain/src/main/kotlin/com/higoods/domain/order/domain/AnswerType.kt
@@ -0,0 +1,6 @@
+package com.higoods.domain.order.domain
+
+enum class AnswerType {
+    SHORT, // 주관식
+    MULTIPLE_CHOICE // 객관식
+}
---
+++ HIGOODS-Domain/src/main/kotlin/com/higoods/domain/order/domain/OrderState.kt
@@ -0,0 +1,6 @@
+package com.higoods.domain.order.domain
+
+enum class OrderState {
+    PENDING, // 입금 대기
+    APPROVAL // 입금 확인
+}
