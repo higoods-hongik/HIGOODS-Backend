ffda3c03def04b3f11d899b177acb6466b75af9c
bryn <cofls6581@naver.com>
2023-08-27 02:09:50 +0900

    test: 유저단 주문 도메인 컨트롤러 테스트 코드 작성

--- HIGOODS-Api/src/test/kotlin/com/higoods/api/order/OrderControllerTest.kt
+++ HIGOODS-Api/src/test/kotlin/com/higoods/api/order/OrderControllerTest.kt
@@ -8,9 +8,12 @@
 // import com.higoods.api.order.dto.request.OrderAnswerDto
 // import com.higoods.api.order.dto.request.OrderCreateRequest
 // import com.higoods.api.order.dto.request.OrderOptionDto
+// import com.higoods.api.order.dto.response.OrderProjectsResponse
 // import com.higoods.api.order.dto.response.OrderResponse
 // import com.higoods.api.order.usecase.OrderCreateUseCase
+// import com.higoods.api.order.usecase.OrderReadUseCase
 // import com.higoods.domain.order.domain.AnswerType
+// import com.higoods.domain.order.domain.OrderState
 // import com.higoods.domain.order.domain.ReceiveType
 // import io.mockk.every
 // import io.mockk.mockk
@@ -22,7 +25,8 @@
 //
 // class OrderControllerTest : BaseControllerTest() {
 //    private val orderCreateUseCase: OrderCreateUseCase = mockk()
-//    override val controller: OrderController = OrderController(orderCreateUseCase)
+//    private val orderReadUseCase: OrderReadUseCase = mockk()
+//    override val controller: OrderController = OrderController(orderCreateUseCase, orderReadUseCase)
 //
 //    init {
 //        test("주문 생성 테스트") {
@@ -57,7 +61,7 @@
 //            every { orderCreateUseCase.execute(1, orderCreateRequest) } returns orderResponse
 //
 //            mockMvc.perform(
-//                RestDocumentationRequestBuilders.post("/v1/orders/{project_id}",1)
+//                RestDocumentationRequestBuilders.post("/v1/orders/{project_id}", 1)
 //                    .contentType(MediaType.APPLICATION_JSON)
 //                    .accept(MediaType.APPLICATION_JSON)
 //            )
@@ -87,6 +91,95 @@
 //                                fieldWithPath("orderAnswers[].answerType").type(JsonFieldType.STRING).description("커스텀 주문폼 정보").optional(),
 //                                fieldWithPath("orderAnswers[].multipleChoiceId").type(JsonFieldType.NUMBER).description("커스텀 주문폼 정보").optional(),
 //                                fieldWithPath("orderAnswers[].shortAnswer").type(JsonFieldType.STRING).description("커스텀 주문폼 정보").optional()
+//                            )
+//                            .responseFields(
+//                                fieldWithPath("orderNo").type(JsonFieldType.STRING).description("주문 번호"),
+//                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+//                                fieldWithPath("studentId").type(JsonFieldType.STRING).description("학번"),
+//                                fieldWithPath("phoneNum").type(JsonFieldType.STRING).description("전화번호"),
+//                                fieldWithPath("receiveType").type(JsonFieldType.STRING).description("상품 수령 방법"),
+//                                fieldWithPath("depositName").type(JsonFieldType.STRING).description("입금자명"),
+//                                fieldWithPath("refundBank").type(JsonFieldType.STRING).description("환불 은행"),
+//                                fieldWithPath("refundAccount").type(JsonFieldType.STRING).description("환불 계좌"),
+//                                fieldWithPath("totalCost").type(JsonFieldType.NUMBER).description("총 금액"),
+//                                fieldWithPath("orderOptions[].optionId").type(JsonFieldType.NUMBER).description("상품 옵션 id"),
+//                                fieldWithPath("orderOptions[].count").type(JsonFieldType.NUMBER).description("상품 옵션 수량"),
+//                                fieldWithPath("orderAnswers[]").description("커스텀 주문폼 정보"),
+//                                fieldWithPath("orderAnswers[].optionId").type(JsonFieldType.NUMBER).description("커스텀 주문폼 정보").optional(),
+//                                fieldWithPath("orderAnswers[].answerType").type(JsonFieldType.STRING).description("커스텀 주문폼 정보").optional(),
+//                                fieldWithPath("orderAnswers[].multipleChoiceId").type(JsonFieldType.NUMBER).description("커스텀 주문폼 정보").optional(),
+//                                fieldWithPath("orderAnswers[].shortAnswer").type(JsonFieldType.STRING).description("커스텀 주문폼 정보").optional()
+//                            )
+//                    )
+//                )
+//        }
+//
+//        test("마이페이지-내 주문 목록 조회") {
+//            val orderProjectResponse = listOf(
+//                OrderProjectsResponse(
+//                    orderId = 1,
+//                    title = "제목",
+//                    titleImage = "image",
+//                    subTitle = "부제목",
+//                    orderState = OrderState.PENDING
+//                )
+//            )
+//
+//            every { orderReadUseCase.findAll() } returns orderProjectResponse
+//
+//            mockMvc.perform(
+//                RestDocumentationRequestBuilders.get("/v1/orders")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .accept(MediaType.APPLICATION_JSON)
+//            )
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andDo(
+//                    MockMvcRestDocumentationWrapper.document(
+//                        "마이페이지-내 주문 목록 조회 API",
+//                        ResourceSnippetParametersBuilder()
+//                            .description("내 주문 목록을 조회합니다.")
+//                            .responseFields(
+//                                fieldWithPath("orderId").type(JsonFieldType.NUMBER).description("주문 id"),
+//                                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+//                                fieldWithPath("titleImage").type(JsonFieldType.STRING).description("이미지"),
+//                                fieldWithPath("subTitle").type(JsonFieldType.STRING).description("부제목"),
+//                                fieldWithPath("orderState").type(JsonFieldType.STRING).description("주문 상태")
+//                            )
+//                    )
+//                )
+//        }
+//
+//        test("내 주문 상세 조회") {
+//            val orderResponse = OrderResponse(
+//                orderNo = "H100001", // 주문번호
+//                name = "이홍익", // 이름
+//                studentId = "B811112", // 학번
+//                phoneNum = "010-1111-2222", // 전화번호
+//                receiveType = ReceiveType.DISTRIBUTION, // 상품 수령 방법, DELIVERY or DISTRIBUTION
+//                refundBank = "신한은행", // 환불 은행
+//                refundAccount = "1234567890", // 환불 계좌
+//                depositName = "이홍익", // 입금자명,
+//                totalCost = 10000, // 총 금액
+//                orderOptions = listOf(OrderOptionDto(1, 2), OrderOptionDto(2, 1)),
+//                orderAnswers = listOf(OrderAnswerDto(1, AnswerType.SHORT, 0, "아니요"), OrderAnswerDto(2, AnswerType.MULTIPLE_CHOICE, 1, ""))
+//
+//            )
+//
+//            every { orderReadUseCase.findById(1) } returns orderResponse
+//
+//            mockMvc.perform(
+//                RestDocumentationRequestBuilders.post("/v1/orders/{order_id}", 1)
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .accept(MediaType.APPLICATION_JSON)
+//            )
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andDo(
+//                    MockMvcRestDocumentationWrapper.document(
+//                        "내 주문 상세 조회 API",
+//                        ResourceSnippetParametersBuilder()
+//                            .description("내 주문 상제 조회 정보입니다.")
+//                            .pathParameters(
+//                                parameterWithName("order_id").description("주문 id")
 //                            )
 //                            .responseFields(
 //                                fieldWithPath("orderNo").type(JsonFieldType.STRING).description("주문 번호"),
